78b9ba5c3da92b19a7d924719551b3de3f539971
xieydd <xieydd@gmail.com>
2017-10-16 16:52:48 +0800

    将MNIST数据转换成TFRecord格式

---
+++ xieydd_Tensorflow_Action_DeepLearning/MNIST_TFRecord.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+#@Description: 将MNIST数据转换成TFRecord格式
+#@author xieydd xieydd@gmail.com  
+#@date 2017-10-16 下午16:50:50 
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np 
