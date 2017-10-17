be151625ead85d250a3a6634bf700a8b48c6c123
xieydd <xieydd@gmail.com>
2017-10-17 16:02:27 +0800

    Tensorflow对于队列的操作

--- xieydd_Tensorflow_Action_DeepLearning/TF_Control_Queue.py
+++ xieydd_Tensorflow_Action_DeepLearning/TF_Control_Queue.py
@@ -5,6 +5,10 @@
 import tensorflow as tf 
 
 #创建一个FIFO队列,指定最多保存两个元素，类型为整数
+'''
+Tensorflow提供两种队列:
+FIFOQueue、RandomHuffleQueue：会将原元素打乱，每次出队列是随机的
+'''
 q = tf.FIFOQueue(2,"int32")
 #与初始化变量一样，这里需要初始化队列两个数为0和10
 init= q.enqueue_many(([0,10],))
