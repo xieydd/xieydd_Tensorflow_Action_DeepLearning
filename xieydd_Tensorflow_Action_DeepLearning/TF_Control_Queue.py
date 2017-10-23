efd540be65be121c19349d90ca2bfbb5e8732b60
xieydd <xieydd@gmail.com>
2017-10-23 10:49:23 +0800

    Tensorflow对于队列的操作

--- xieydd_Tensorflow_Action_DeepLearning/TF_Control_Queue.py
+++ xieydd_Tensorflow_Action_DeepLearning/TF_Control_Queue.py
@@ -1,9 +1,8 @@
 # -*- coding:utf-8 -*-
-#@Description: Tensorflow对于队列的操作 
-#@author xieydd xieydd@gmail.com  
+#@Description: Tensorflow对于队列的操作
+#@author xieydd xieydd@gmail.com
 #@date 2017-10-17 下午15:37:23
-import tensorflow as tf 
-
+import tensorflow as tf
 #创建一个FIFO队列,指定最多保存两个元素，类型为整数
 '''
 Tensorflow提供两种队列:
@@ -18,9 +17,8 @@ x = q.dequeue()
 y = x+1
 #将加1后的值重新加入队列
 q_inc = q.enqueue([y])
-
-with tf.Session as sess:
-	init.run()
-	for _ in range(5):
-		v,_ = sess.run([x,q_inc])
-		print(x)
+with tf.Session() as sess:
+    init.run()
+    for _ in range(5):
+        v,_ = sess.run([x,q_inc])
+        print(v)
